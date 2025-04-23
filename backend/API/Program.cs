15f58f3491e21a18efd3a5e8d80bffbcc22d53f4
Ghost4Man <kosik.dan@gmail.com>
2025-04-23 16:37:39 +0200

    Add tags to endpoints (to enable grouping in Scalar)

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -50,7 +50,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World");
+app.MapGet("/", () => "Hello World").WithTags("all endpoints");
 
 app.RegisterEndpointDefinitions();
 
